0179df19f10ff3683753961057e14413696da19e
FrankRogalski <rogalski.frank@web.de>
2020-06-06 16:53:59 +0200

    added User Agent

--- Commands/CanIHazDadJoke.py
+++ Commands/CanIHazDadJoke.py
@@ -1,13 +1,16 @@
 import requests
 
 class CanIHazDadJoke:
+    def __init__(self):
+        self.headers = {"Accept": "application/json", "User-Agent": "Discord bot https://github.com/FrankRogalski/Riccchard"}
+
     def use(self, msg):
         words = msg.content.split()
         if len(words) <= 1:
-            response = requests.get("https://icanhazdadjoke.com", headers={"Accept": "application/json"})
+            response = requests.get("https://icanhazdadjoke.com", headers=self.headers)
             return response.json()["joke"], True
         else:
-            response = requests.get("https://icanhazdadjoke.com/search", params={"limit": 1, "term": " ".join(words[1:])}, headers={"Accept": "application/json"})
+            response = requests.get("https://icanhazdadjoke.com/search", params={"limit": 1, "term": " ".join(words[1:])}, headers=self.headers)
             return (res[0]["joke"], True) if len(res := response.json()["results"]) > 0 else ("No jokes found", False)
         
     def get_keyword(self):
